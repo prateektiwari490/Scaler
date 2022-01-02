a1fe3a451b97d24ea56f686fa70e9081dfe268f4
prateektiwari490 <prateektiwari490@gmail.com>
2022-01-03 03:28:32 +0530

    #

--- Class_31_Maths_GCD/ALL_GCD_PAIR.java
+++ Class_31_Maths_GCD/ALL_GCD_PAIR.java
@@ -8,18 +8,25 @@ import java.util.List;
 public class ALL_GCD_PAIR {
 
 	public static void main(String[] args) {
-		List<Integer> A = Arrays.asList(46, 1, 2, 1, 1, 1, 5, 45, 1, 1, 2, 5, 1, 40, 1, 1, 1, 1, 1, 1, 1, 1, 1, 31, 1);
-		
+//		List<Integer> A = Arrays.asList(46, 1, 2, 1, 1, 1, 5, 45, 1, 1, 2, 5, 1, 40, 1, 1, 1, 1, 1, 1, 1, 1, 1, 31, 1);
+		List<Integer> A = Arrays.asList(2, 2, 2, 2, 8, 2, 2, 2, 10);
+
 		int n = A.size();
-		Collections.sort(A,Collections.reverseOrder());
-		int num = (int) Math.sqrt(n);
-		ArrayList<Integer> ans = new ArrayList<>();
-		int i = 0;
-		while(i<num) {
-			ans.add(A.get(i));
-			i++;
+		int interval = (int) Math.sqrt(n);
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		int max = 0;
+		int count = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (A.get(i) > max)
+				max = A.get(i);
+			count++;
+			if (count == interval) {
+				ans.add(max);
+				max = 0;
+				count = 0;
+			}
 		}
-		
 		System.out.println(ans);
 	}
 
--- Class_56_Queues/Task_Scheduling.java
+++ Class_56_Queues/Task_Scheduling.java
@@ -1,8 +1,8 @@
 package Class_56_Queues;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Task_Scheduling {
@@ -10,27 +10,27 @@ public class Task_Scheduling {
 	public static void main(String[] args) {
 		List<Integer> A = Arrays.asList(2, 3, 1, 5, 4);
 		List<Integer> B = Arrays.asList(1, 3, 5, 4, 2);
-		
+
 		int n = A.size();
-		
-		Deque<Integer> q = new LinkedList<Integer>();
-		
-		for(int i=0;i<n;i++) {
-			q.add(A.get(i));
-		}
-		
+
+		Deque<Integer> deque = new ArrayDeque<>();
 		int ans = 0;
-		for(int i=0;i<n;i++) {
-			while(B.get(i) != q.peekFirst()) {
-				int temp = q.peekFirst();
-				q.removeFirst();
+
+		for (int i = 0; i < n; i++) {
+			deque.addLast(A.get(i));
+		}
+
+		for (int i = 0; i < n; i++) {
+			while (B.get(i) != deque.peekFirst()) {
+				int temp = deque.peekFirst();
+				deque.removeFirst();
 				ans++;
-				q.add(temp);
+				deque.addLast(temp);
 			}
 			ans++;
-			q.removeFirst();
+			deque.removeFirst();
 		}
-		
+
 		System.out.println(ans);
 	}
 

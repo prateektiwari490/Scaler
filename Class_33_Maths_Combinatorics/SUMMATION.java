1c15fd5efb51115a7ac4791ce43ad7dd19582280
prateektiwari490 <prateektiwari490@gmail.com>
2022-01-04 20:15:44 +0530

    SUMMATION

--- Class_33_Maths_Combinatorics/SUMMATION.java
+++ Class_33_Maths_Combinatorics/SUMMATION.java
@@ -1,41 +1,31 @@
 package Class_33_Maths_Combinatorics;
 
 public class SUMMATION {
-	
-	static void initialize(int[][] l,int A, int m)
-    {
-         l[0][0] = 1;
-        for (int i = 1; i < A+1; i++) {
-            l[i][0] = 1;
-            for (int j = 1; j < i+1; j++) {
-                 l[i][j] = (l[i - 1][j - 1]%m + l[i - 1][j]%m)%m;
-            }
-        }
-    }
-	
-	static int pow2(int r,int m) {
-		int pow = 1;
-		int i=1;
-		while(i<=r) {
-			pow *= 2;
-			pow = pow%m;
+	// n*(n-1)*3^(n-2)
+
+	static int calc(int n, int mod) {
+		long ans = (long) (((n % mod * (n - 1) % mod) % mod * fast_power(3, n - 2, mod) % mod) % mod);
+		return (int) ans % mod;
+	}
+
+	static long fast_power(long A, long B, long mod) {
+		long ans = 1;
+		while (B > 0) {
+			if ((B & 1) == 1) {
+				ans = (ans * A) % mod;
+			}
+			A = (A % mod * A % mod) % mod;
+			B = B >> 1;
 		}
-		return (1<<r);
+		return (ans % mod);
 	}
 
 	public static void main(String[] args) {
 		int A = 39;
 		int m = 1000000007;
-		
-		int l[][] = new int[A+1][A+1];
-        initialize(l,A, m);
-        System.out.println();
-        long sum = 0;
-        for(int i=0;i<=A;i++) {
-        	sum += (l[A][i]%m * i%m * (i-1)%m * pow2(i-2,m)%m)%m;
-        	System.out.println(sum + " " + l[A][i]%m + " " + i%m + " " + (i-1)%m + " " + pow2(i-2,m));
-        }
-        System.out.println(sum);
+
+		int ans = calc(A, m);
+		System.out.println(ans);
 	}
 
 }

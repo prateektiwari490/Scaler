2f2c946e6aa4ab5c1aaed8a733e64e0bac93614f
prateektiwari490 <prateektiwari490@gmail.com>
2022-01-04 19:06:12 +0530

    ADD OR NOT

--- Class_40_Searching_II/ADD_OR_NOT.java
+++ Class_40_Searching_II/ADD_OR_NOT.java
@@ -7,48 +7,48 @@ import java.util.List;
 
 public class ADD_OR_NOT {
 
-	static boolean check(int count, List<Integer> A, int B, int i, int[] prefix) {
-
-		if (A.get(i) * count - (prefix[i + 1] - prefix[i - count + 1]) <= B) {
-			return true;
-		}
-
-		return false;
-	}
-
 	public static void main(String[] args) {
 		List<Integer> A = Arrays.asList(3, 1, 2, 2, 1);
 		int B = 3;
-
+		
 		int n = A.size();
-		Collections.sort(A);
-		int[] prefix = new int[100005];
-		for (int i = 0; i < n; i++) {
-			prefix[i + 1] = prefix[i] + A.get(i);
-		}
 
-		ArrayList<Integer> ans = new ArrayList<>();
-		ans.add(-1);
-		ans.add(-1);
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < n; i++) {
-			int low = 1;
-			int high = i + 1;
-			while (low <= high) {
-				int count = (low + high) / 2;
-				if (check(count, A, B, i, prefix)) {
-					max = count;
-					low = count + 1;
-				} else {
-					high = count - 1;
-				}
-			}
-			if(ans.get(0) < max) {
-				ans.set(0, max);
-				ans.set(1, A.get(i));
-			}
-		}
-		System.out.println(ans);
+		Collections.sort(A);
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+
+        long[] prefix = new long[n+1];
+        prefix[0] = 0;
+        for(int i=1; i<=n; i++)
+        {
+            prefix[i] = prefix[i-1]+A.get(i-1);
+        }
+
+        int max_count = -1;
+        int max_num = -1;
+        for(int i=0; i<n; i++)
+        {
+            int low = 1;
+            int high = i+1;
+            while(low<=high)
+            {
+                int mid = (low+high)>>1;
+                long sum = prefix[i+1]-prefix[i+1-mid];
+                if(1L*A.get(i)*mid - sum <= B)
+                {
+                    if(mid > max_count)
+                    {
+                        max_count = mid;
+                        max_num = A.get(i);
+                    }
+                    low = mid+1;
+                }
+                else
+                    high = mid-1;
+            }
+        }
+        ans.add(max_count);
+        ans.add(max_num);
+        System.out.println(ans);
 	}
 
 }

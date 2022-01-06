a083187eaf666e9bc0aaddd53a9aa19a058e0864
prateektiwari490 <prateektiwari490@gmail.com>
2022-01-07 03:40:42 +0530

    Infix to Postfix

--- Class_53_Stacks_II/Infix_to_Postfix.java
+++ Class_53_Stacks_II/Infix_to_Postfix.java
@@ -8,42 +8,39 @@ public class Infix_to_Postfix {
 	public static void main(String[] args) {
 		String A = "a+b*(c^d-e)^(f+g*h)-i"; // abcd^e-fgh*+^*+i-
 
-		int n = A.length();
-
-		Stack<Character> stack = new Stack<>();
-
-		String ans = "";
-
+		StringBuilder sb = new StringBuilder();
+		Stack<Character> stack = new Stack<Character>();
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		map.put('^', 1);
 		map.put('/', 2);
 		map.put('*', 2);
 		map.put('+', 3);
 		map.put('-', 3);
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < A.length(); i++) {
 			char ch = A.charAt(i);
 			if (ch >= 'a' && ch <= 'z')
-				ans += ch;
+				sb.append(ch);
 			else if (ch == '(')
 				stack.push(ch);
 			else if (ch == ')') {
 				while (stack.peek() != '(') {
-					ans += stack.peek();
+					sb.append(stack.peek());
 					stack.pop();
 				}
 				stack.pop();
 			} else {
 				while (!stack.isEmpty() && stack.peek() != '(' && map.get(stack.peek()) <= map.get(ch)) {
-					ans += stack.peek();
+					sb.append(stack.peek());
 					stack.pop();
 				}
 				stack.push(ch);
 			}
 		}
 		while (!stack.isEmpty()) {
-			ans += stack.peek();
+			sb.append(stack.peek());
 			stack.pop();
 		}
-		System.out.println(ans);
+
+		System.out.println(sb.toString());
 	}
 }

c02a35f3f3f0712956e77c309b58db50dd8f9068
prateektiwari490 <prateektiwari490@gmail.com>
2022-03-05 21:39:16 +0530

    Path_in_Directed_Graph

---
+++ Class_82_Graph_1/Path_in_Directed_Graph.java
@@ -0,0 +1,24 @@
+package Class_82_Graph_1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+public class Path_in_Directed_Graph {
+
+	public static void main(String[] args) {
+//		 A = 5
+//				 B = [  [1, 2] 
+//				        [4, 1] 
+//				        [2, 4] 
+//				        [3, 4] 
+//				        [5, 2] 
+//				        [1, 3] ]
+		int A = 5;
+		List<List<Integer>> B = Arrays.asList(Arrays.asList(1, 2), Arrays.asList(4, 1), Arrays.asList(2, 4),
+				Arrays.asList(3, 4), Arrays.asList(5, 2), Arrays.asList(1, 3));
+		
+		
+	}
+
+}
